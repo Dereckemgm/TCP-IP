938af8d44ddf95a320593e7ad77f40256cf75aa8
Dereck <dexcka3@gmail.com>
2024-02-23 12:09:55 -0500

    Primer commit
    Se agrega el proyecto de servidor y cliente para calcular el IMC

---
+++ ClienteTcpImc/src/johnarrieta/imc/cliente/Principal.java
@@ -0,0 +1,19 @@
+package johnarrieta.imc.cliente;
+
+import johnarrieta.imc.cliente.vistas.VentanaPrincipal;
+
+/**
+ *
+ * @author JOHN CARLOS ARRIETA ARRIETA
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+}
---
+++ ServidorTcpImc/src/johnarrieta/imc/Principal.java
@@ -0,0 +1,13 @@
+package johnarrieta.imc;
+import johnarrieta.imc.vistas.VentanaPrincipal;
+
+public class Principal {
+/**
+* @param args the command line arguments
+*/
+public static void main(String[] args) {
+VentanaPrincipal v = new VentanaPrincipal();
+v.setLocationRelativeTo(null);
+v.setVisible(true);
+}
+}
